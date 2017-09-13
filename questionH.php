c1dc62e84a8c3044c87c357d8dac6f87a7211641
Akash Badone <akashbadone0@gmail.com>
2017-09-13 12:01:24 +0530

    Add files via upload

---
+++ questionH.php
@@ -0,0 +1,20 @@
+<?php session_start();
+ require("header.php");
+require("checkUser.php")?>
+
+
+<?php
+if(isset($_POST['submit'])){
+$stid = $_POST['stid'];
+$ta = $_POST['ta'];
+$hd=$_POST['head'];
+$uid = $_SESSION["uid"];
+
+$sql="INSERT INTO question ( `heading`,`question_detail`, `user_id`, `subtopic_id`) VALUES ( '$hd','$ta', '$uid', '$stid')";
+$result=ExecuteNonquery($sql);
+
+if ($result == 1)
+	header ("location:questions.php?id=$stid");
+}
+?>
+<?php require("footer.php")?>
